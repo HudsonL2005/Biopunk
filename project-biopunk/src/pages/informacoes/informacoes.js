c774d16596386b555502f25603a1e4886a30d676
Gustavo-Vieira-Unasp <fonseca_vieira@educadventista.org>
2025-09-01 13:52:04 -0300

    Rotas - informações e personagens

---
+++ project-biopunk/src/pages/informacoes/informacoes.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+function Informacoes() {
+  return (
+    <h1>Informações</h1>
+  );
+}
+
+export default Informacoes;
---
+++ project-biopunk/src/pages/personagens/personagens.js
@@ -0,0 +1,12 @@
+import React, {useEffect} from 'react';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+
+function Personagens() {
+  return (
+    <h1>Personagens</h1>
+  );
+}
+
+export default Personagens;
